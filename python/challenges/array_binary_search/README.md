d67c73329747e305417fd870303f9c9915dbbc1d
Rawan199812 <rawan.13579@icloud.com>
2021-05-08 16:16:52 +0300

    fix the code and add README file

---
+++ python/challenges/array_binary_search/README.md
@@ -0,0 +1,8 @@
+## BinarySearch
+Title: array-binary-search
+
+Challenge details:  
+BinarySearch function takes in 2 parameters: a sorted array and the search key. (Without any methods) return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
+
+whiteboard:
+![array-binary-search](binary.jpg)
--- python/challenges/array_binary_search/test_array_binary_search.py
+++ python/challenges/array_binary_search/test_array_binary_search.py
@@ -1,5 +1,5 @@
 # from array_binary_search import __version__
-from array_binary_search.array_binary_search import BinarySearch
+from array_binary_search import BinarySearch
 
 
 # def test_version():
@@ -12,7 +12,12 @@ def test_one():
     assert actual == expected
 
 
-def test_two():
+def test_element_does_not_exist():
     actual = BinarySearch([11,22,33,44,55,66,77], 90)
     expected = -1
+    assert actual == expected
+
+def test_two():
+    actual = BinarySearch([4,8,15,16,23,42], 23)
+    expected = 4
     assert actual == expected
